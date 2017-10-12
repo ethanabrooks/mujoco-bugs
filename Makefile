d6c5700b6b15a6460c4cf874ad38ea8ab9b082c3
Ethan Brooks <ethanabrooks@gmail.com>
2017-10-12 12:19:09 -0400

    added Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
+src=src
+build=build
+
+all:
+	mkdir -p $(build)
+	gcc -fPIC $(src)/glfwbug.cpp -I/home/ethanbro/.mujoco/mjpro150/include -L${HOME}/.mujoco/mjpro150/bin -lmujoco150 -lglewosmesa -lOSMesa -lGL -l:libglfw.so.3 -o $(build)/glfw-bug
 clean:
-	rm -rf build/
-	rm *.c *.so
+	rm -rf $(build)/
+	rm "*.c" "*.so"
+
 
--- src/glfwbug.cpp
+++ src/glfwbug.cpp
@@ -7,9 +7,11 @@ mjrContext c;
 char error[300];
 
 int main(int argc, const char** argv) {
-  m = mj_loadXML(argv[1], 0, error, 1000);
-  glfwInit();
-  glfwCreateWindow(800, 800, "Invisible window", NULL, NULL);
   mjr_defaultContext(&c);
+  glfwInit();
+  GLFWwindow* window = glfwCreateWindow(800, 800, "Invisible window", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  m = mj_loadXML(argv[1], 0, error, 1000);
   mjr_makeContext(m, &c, mjFONTSCALE_150);
+  mjr_freeContext(&c);
 }
