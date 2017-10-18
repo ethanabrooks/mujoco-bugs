f89c94cc774de7c3c98e03e62ca05eaeed06dec1
Ethan Brooks <ethanabrooks@gmail.com>
2017-10-18 14:07:46 -0400

    improved makefile and moved stuff around

--- Makefile
+++ Makefile
@@ -25,8 +25,7 @@ clean:
 
 render:
 	mkdir -p $(build)
+	rm rgb.out video.mp4
 	g++ -O2 -I/home/ethanbro/.mujoco/mjpro150/include -L${HOME}/.mujoco/mjpro150/bin -std=c++11 -mavx src/render.cpp -lmujoco150 -lGL -lglew ${HOME}/.mujoco/mjpro150/bin/libglfw.so.3 -o $(build)/render
-
-render2:
-	mkdir -p $(build)
-	g++ -O2 -I/home/ethanbro/.mujoco/mjpro150/include -L${HOME}/.mujoco/mjpro150/bin -std=c++11 -mavx src/render2.cpp -lmujoco150 -lGL -lglew ${HOME}/.mujoco/mjpro150/bin/libglfw.so.3 -o $(build)/render
+	./build/render
+	ffmpeg -y -f rawvideo -pixel_format rgb24 -video_size 1718x706 -framerate 60 -i rgb.out -vf "vflip" video.mp4
--- src/render.cpp
+++ src/render.cpp
@@ -1,9 +1,3 @@
-//---------------------------------//
-//  This file is part of MuJoCo    //
-//  Written by Emo Todorov         //
-//  Copyright (C) 2017 Roboti LLC  //
-//---------------------------------//
-
 
 #include "mujoco.h"
 #include "stdio.h"
@@ -24,24 +18,20 @@ mjvCamera cam;
 mjvOption opt;
 mjrContext con;
 
+
 //-------------------------------- main function ----------------------------------------
 
 int main(int argc, const char** argv)
 {
-    // parse numeric arguments
-    double duration = 10, fps = 30;
-
-    // initialize OpenGL and MuJoCo
-    //initOpenGL();
     if( !glfwInit() )
         mju_error("Could not initialize GLFW");
 
     // create invisible window, single-buffered
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
-    GLFWwindow* offscreen = glfwCreateWindow(800, 800, "Invisible window", NULL, NULL);
-    if( !offscreen )
-        mju_error("Could not create offscreen GLFW window");
+    //glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    //glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
+    //GLFWwindow* offscreen = glfwCreateWindow(800, 800, "Invisible window", NULL, NULL);
+    //if( !offscreen )
+        //mju_error("Could not create GLFW window");
 
     // create visible window, double-buffered
     glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
@@ -50,19 +40,13 @@ int main(int argc, const char** argv)
     if( !window )
         mju_error("Could not create GLFW window");
 
-    // set context to offscreen window
-    //glfwMakeContextCurrent(window);
-    glfwMakeContextCurrent(offscreen);
-
+    glfwMakeContextCurrent(window);
     mj_activate("mjkey.txt");
 
-    // load and compile
-    char error[1000] = "Could not load binary model";
+    char error[1000] = "Could not load xml model";
     m = mj_loadXML("humanoid.xml", 0, error, 1000);
     if( !m )
         mju_error_s("Load model error: %s", error);
-
-    // make data, run one computation to initialize all fields
     d = mj_makeData(m);
     mj_forward(m, d);
 
@@ -73,21 +57,24 @@ int main(int argc, const char** argv)
     mjr_defaultContext(&con);
     mjr_makeContext(m, &con, 200);
 
-    cam.type = mjCAMERA_FIXED; 
-    cam.fixedcamid = 1; 
+    // select camera
+    //cam.fixedcamid = 0;
+    //cam.type = mjCAMERA_FIXED;
 
     // get size of active renderbuffer
-    mjrRect rect =  mjr_maxViewport(&con);
-    int W = rect.width;
-    int H = rect.height;
+    //mjrRect rect =  mjr_maxViewport(&con);
+    //int W = rect.width;
+    //int H = rect.height;
 
-    mjrRect viewport = {0, 0, 0, 0};
-    glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
+    // get size of window
+    mjrRect window_rect = {0, 0, 0, 0};
+    glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
+    int W = window_rect.width;
+    int H = window_rect.height;
 
     // allocate rgb and depth buffers
     unsigned char* rgb = (unsigned char*)malloc(3*W*H);
-    float* depth = (float*)malloc(sizeof(float)*W*H);
-    if( !rgb || !depth )
+    if( !rgb )
         mju_error("Could not allocate buffers");
 
     // create output rgb file
@@ -96,68 +83,39 @@ int main(int argc, const char** argv)
         mju_error("Could not open rgbfile for writing");
 
     // main loop
-    double frametime = 0;
-    int framecount = 0;
-    for( int i = 0; i < 100; i++)
-    {
-
-      // update abstract scene
+    for( int i = 0; i < 100; i++) {
       mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-
-      // switch to offscreen context 
-      glfwMakeContextCurrent(offscreen);
 
       // set rendering to offscreen buffer
-      mjr_setBuffer(mjFB_OFFSCREEN, &con);
-      if( con.currentBuffer!=mjFB_OFFSCREEN )
-          printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
+      //glfwMakeContextCurrent(offscreen);
+      //mjr_setBuffer(mjFB_OFFSCREEN, &con);
+      //if( con.currentBuffer!=mjFB_OFFSCREEN )
+          //printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
 
-      // render scene in offscreen buffer
-      mjr_render(rect, &scn, &con);
-
-      // read rgb and depth buffers
-      mjr_readPixels(rgb, depth, rect, &con);
-
-      // write rgb image to file
+      // write offscreen-rendered pixels to file
+      mjr_render(window_rect, &scn, &con);
+      mjr_readPixels(rgb, NULL, window_rect, &con);
       fwrite(rgb, 3, W*H, fp);
 
-      /*
-      cam.type = mjCAMERA_FREE; 
-      cam.fixedcamid = -1;
-
-      // update abstract scene
-      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-
-      // make context current
-      glfwMakeContextCurrent(window);
-
-      // set rendering to window buffer
-      mjr_setBuffer(mjFB_WINDOW, &con);
-      if( con.currentBuffer!=mjFB_WINDOW )
-          printf("Warning: unable to select window framebuffer.\n");
-
-      // render scene in window buffer
-      mjr_render(viewport, &scn, &con);
-      */
+      // render to visible window
+      //glfwMakeContextCurrent(window);
+      //mjr_setBuffer(mjFB_WINDOW, &con);
+      //if( con.currentBuffer!=mjFB_WINDOW )
+          //printf("Warning: rendering not supported, using default/window framebuffer\n");
+      //mjr_render(window_rect, &scn, &con);
+      glfwSwapBuffers(window);
 
       // advance simulation
       mj_step(m, d);
-      //glfwSwapBuffers(window);
     }
+    printf("ffmpeg -y -f rawvideo -pixel_format rgb24 -video_size %dx%d -framerate 60 -i rgb.out -vf \"vflip\" video.mp4", W, H);
 
-    // close file, free buffers
     fclose(fp);
     free(rgb);
-    free(depth);
-
-    // close MuJoCo and OpenGL
     mj_deleteData(d);
     mj_deleteModel(m);
     mjr_freeContext(&con);
     mjv_freeScene(&scn);
     mj_deactivate();
-    //glfwTerminate();
-
     return 1;
 }
-
