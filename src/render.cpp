9854a2b05e084dcbc4eb67ef1c5c78384cdaa003
Ethan Brooks <ethanabrooks@gmail.com>
2017-10-18 14:15:35 -0400

    overwrote render2 with render

--- src/render.cpp
+++ src/render.cpp
@@ -26,13 +26,6 @@ int main(int argc, const char** argv)
     if( !glfwInit() )
         mju_error("Could not initialize GLFW");
 
-    // create invisible window, single-buffered
-    //glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-    //glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
-    //GLFWwindow* offscreen = glfwCreateWindow(800, 800, "Invisible window", NULL, NULL);
-    //if( !offscreen )
-        //mju_error("Could not create GLFW window");
-
     // create visible window, double-buffered
     glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
     glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
@@ -57,15 +50,6 @@ int main(int argc, const char** argv)
     mjr_defaultContext(&con);
     mjr_makeContext(m, &con, 200);
 
-    // select camera
-    //cam.fixedcamid = 0;
-    //cam.type = mjCAMERA_FIXED;
-
-    // get size of active renderbuffer
-    //mjrRect rect =  mjr_maxViewport(&con);
-    //int W = rect.width;
-    //int H = rect.height;
-
     // get size of window
     mjrRect window_rect = {0, 0, 0, 0};
     glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
@@ -86,29 +70,14 @@ int main(int argc, const char** argv)
     for( int i = 0; i < 100; i++) {
       mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
 
-      // set rendering to offscreen buffer
-      //glfwMakeContextCurrent(offscreen);
-      //mjr_setBuffer(mjFB_OFFSCREEN, &con);
-      //if( con.currentBuffer!=mjFB_OFFSCREEN )
-          //printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
-
       // write offscreen-rendered pixels to file
       mjr_render(window_rect, &scn, &con);
       mjr_readPixels(rgb, NULL, window_rect, &con);
       fwrite(rgb, 3, W*H, fp);
 
-      // render to visible window
-      //glfwMakeContextCurrent(window);
-      //mjr_setBuffer(mjFB_WINDOW, &con);
-      //if( con.currentBuffer!=mjFB_WINDOW )
-          //printf("Warning: rendering not supported, using default/window framebuffer\n");
-      //mjr_render(window_rect, &scn, &con);
       glfwSwapBuffers(window);
-
-      // advance simulation
       mj_step(m, d);
     }
-    printf("ffmpeg -y -f rawvideo -pixel_format rgb24 -video_size %dx%d -framerate 60 -i rgb.out -vf \"vflip\" video.mp4", W, H);
 
     fclose(fp);
     free(rgb);
