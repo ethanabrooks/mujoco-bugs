b474eabde19d07b222d35d1cd9610c5787655241
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-13 20:58:47 -0500

    refactored render.cpp into functions

--- src/render.cpp
+++ src/render.cpp
@@ -18,11 +18,7 @@ mjvCamera cam;
 mjvOption opt;
 mjrContext con;
 
-
-//-------------------------------- main function ----------------------------------------
-
-int main(int argc, const char** argv)
-{
+GLFWwindow* initGlfw() {
     if( !glfwInit() )
         mju_error("Could not initialize GLFW");
 
@@ -34,10 +30,14 @@ int main(int argc, const char** argv)
         mju_error("Could not create GLFW window");
 
     glfwMakeContextCurrent(window);
+    return window;
+}
+
+int initMujoco(char const* filepath) {
     mj_activate("mjkey.txt");
 
     char error[1000] = "Could not load xml model";
-    m = mj_loadXML("humanoid.xml", 0, error, 1000);
+    m = mj_loadXML(filepath, 0, error, 1000);
     if( !m )
         mju_error_s("Load model error: %s", error);
     d = mj_makeData(m);
@@ -49,17 +49,61 @@ int main(int argc, const char** argv)
     mjv_defaultOption(&opt);
     mjr_defaultContext(&con);
     mjr_makeContext(m, &con, 200);
+    return 0;
+}
 
-    // get size of window
-    //int W = window_rect.width;
-    //int H = window_rect.height;
+int renderOffscreen(unsigned char* rgb, int height, int width) {
+      mjrRect viewport = {0, 0, height, width};
+      cam.fixedcamid = 0;
+      cam.type = mjCAMERA_FIXED;
+      // write offscreen-rendered pixels to file
+      mjr_setBuffer(mjFB_OFFSCREEN, &con);
+      if( con.currentBuffer!=mjFB_OFFSCREEN )
+          printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
+      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+      mjr_render(viewport, &scn, &con);
+      mjr_readPixels(rgb, NULL, viewport, &con);
+      return 0;
+}
 
-    mjrRect viewport =  mjr_maxViewport(&con);
-    int W = viewport.width;
-    int H = viewport.height;
+int renderOnscreen(GLFWwindow* window) {
+      mjrRect window_rect = {0, 0, 0, 0};
+      glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
+      cam.fixedcamid = -1;
+      cam.type = mjCAMERA_FREE;
+      mjr_setBuffer(mjFB_WINDOW, &con);
+      if( con.currentBuffer!=mjFB_WINDOW )
+          printf("Warning: window rendering not supported\n");
+      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+      mjr_render(window_rect, &scn, &con);
+      glfwSwapBuffers(window);
+      return 0;
+}
+
+int close(FILE* fp, unsigned char* rgb) {
+    fclose(fp);
+    free(rgb);
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    mjr_freeContext(&con);
+    mjv_freeScene(&scn);
+    mj_deactivate();
+}
+
+
+//-------------------------------- main function ----------------------------------------
+
+int main(int argc, const char** argv)
+{
+    int H = 800;
+    int W = 800;
+    char const* filepath = "humanoid.xml";
+
+    GLFWwindow* window = initGlfw();
+    initMujoco(filepath);
 
     // allocate rgb and depth buffers
-    unsigned char* rgb = (unsigned char*)malloc(3*W*H);
+    unsigned char* rgb = (unsigned char*)malloc(3*H*W);
     if( !rgb )
         mju_error("Could not allocate buffers");
 
@@ -70,39 +114,12 @@ int main(int argc, const char** argv)
 
     // main loop
     for( int i = 0; i < 500; i++) {
-
-      mjrRect window_rect = {0, 0, 0, 0};
-      glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
-      cam.fixedcamid = 0;
-      cam.type = mjCAMERA_FIXED;
-      // write offscreen-rendered pixels to file
-      mjr_setBuffer(mjFB_OFFSCREEN, &con);
-      if( con.currentBuffer!=mjFB_OFFSCREEN )
-          printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
-      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-      mjr_render(viewport, &scn, &con);
-      mjr_readPixels(rgb, NULL, viewport, &con);
-      fwrite(rgb, 3, W*H, fp);
-
-      cam.fixedcamid = -1;
-      cam.type = mjCAMERA_FREE;
-      mjr_setBuffer(mjFB_WINDOW, &con);
-      if( con.currentBuffer!=mjFB_WINDOW )
-          printf("Warning: window rendering not supported\n");
-      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-      mjr_render(window_rect, &scn, &con);
-
-      glfwSwapBuffers(window);
+      renderOffscreen(rgb, H, W);
+      fwrite(rgb, 3, H * W, fp);
+      renderOnscreen(window);
       mj_step(m, d);
     }
     printf("ffmpeg -f rawvideo -pixel_format rgb24 -video_size %dx%d -framerate 60 -i rgb.out -vf 'vflip' video.mp4\n", H, W);
 
-    fclose(fp);
-    free(rgb);
-    mj_deleteData(d);
-    mj_deleteModel(m);
-    mjr_freeContext(&con);
-    mjv_freeScene(&scn);
-    mj_deactivate();
-    return 1;
+    return 0;
 }
