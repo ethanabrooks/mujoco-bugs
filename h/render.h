4c57bf8a85d8ea7a728a35a70f50530fb2293341
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-13 23:34:13 -0500

    awesome

---
+++ h/render.h
@@ -0,0 +1,27 @@
+#ifndef _RENDER_H
+#define _RENDER_H
+
+#include "mujoco.h"
+#include "glfw3.h"
+
+int test;
+mjModel* m;
+mjData* d;
+
+// MuJoCo visualization
+mjvScene scn;
+mjvCamera cam;
+mjvOption opt;
+mjrContext con;
+
+GLFWwindow* initGlfw();
+mjModel* loadModel(const char* filepath);
+int initMujoco(mjModel* m, mjData* d, mjvScene* scn, 
+    mjvCamera* cam, mjvOption* opt, mjrContext* con);
+int renderOffscreen(unsigned char* rgb, int height, int width, 
+    mjModel* m, mjData* d, mjvCamera* cam);
+int renderOnscreen(GLFWwindow* window, mjModel* m, mjData* d, 
+    mjvScene* scn, mjrContext* con, mjvCamera* cam, mjvOption* opt);
+int closeMujoco(mjModel* m, mjData* d, mjrContext* con, mjvScene* scn);
+
+#endif
--- src/render.c
+++ src/render.c
@@ -7,16 +7,6 @@
 
 //-------------------------------- global data ------------------------------------------
 
-// MuJoCo model and data
-mjModel* m = 0;
-mjData* d = 0;
-
-// MuJoCo visualization
-mjvScene scn;
-mjvCamera cam;
-mjvOption opt;
-mjrContext con;
-
 GLFWwindow* initGlfw() {
     if( !glfwInit() )
         mju_error("Could not initialize GLFW");
@@ -32,60 +22,57 @@ GLFWwindow* initGlfw() {
     return window;
 }
 
-int initMujoco(char const* filepath) {
-    mj_activate("mjkey.txt");
-
+mjModel* loadModel(const char* filepath) {
     char error[1000] = "Could not load xml model";
-    m = mj_loadXML(filepath, 0, error, 1000);
+    mjModel* m = mj_loadXML(filepath, 0, error, 1000);
     if( !m )
         mju_error_s("Load model error: %s", error);
-    d = mj_makeData(m);
-    mj_forward(m, d);
-
-    // initialize MuJoCo visualization
-    mjv_makeScene(&scn, 1000);
-    mjv_defaultCamera(&cam);
-    mjv_defaultOption(&opt);
-    mjr_defaultContext(&con);
-    mjr_makeContext(m, &con, 200);
-    return 0;
+    return m;
 }
 
-int renderOffscreen(unsigned char* rgb, int height, int width) {
+int initMujoco(mjModel* m, mjData* d, mjvScene* scn, 
+    mjvCamera* cam, mjvOption* opt, mjrContext* con) {
+      mj_forward(m, d);
+      mjv_makeScene(scn, 1000);
+      mjv_defaultCamera(cam);
+      mjv_defaultOption(opt);
+      mjr_defaultContext(con);
+      mjr_makeContext(m, con, 200);
+}
+
+int renderOffscreen(unsigned char* rgb, int height, int width, 
+    mjModel* m, mjData* d, mjvCamera* cam) {
       mjrRect viewport = {0, 0, height, width};
       cam.fixedcamid = 0;
       cam.type = mjCAMERA_FIXED;
       // write offscreen-rendered pixels to file
-      mjr_setBuffer(mjFB_OFFSCREEN, &con);
+      mjr_setBuffer(mjFB_OFFSCREEN, con);
       if( con.currentBuffer!=mjFB_OFFSCREEN )
           printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
-      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-      mjr_render(viewport, &scn, &con);
-      mjr_readPixels(rgb, NULL, viewport, &con);
-      return 0;
+      mjv_updateScene(m, d, opt, NULL, cam, mjCAT_ALL, scn);
+      mjr_render(viewport, scn, con);
+      mjr_readPixels(rgb, NULL, viewport, con);
 }
 
-int renderOnscreen(GLFWwindow* window) {
-      mjrRect window_rect = {0, 0, 0, 0};
-      glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
+int renderOnscreen(GLFWwindow* window, mjModel* m, mjData* d, 
+    mjvScene* scn, mjrContext* con, mjvCamera* cam, mjvOption* opt) {
+      mjrRect rect = {0, 0, 0, 0};
+      glfwGetFramebufferSize(window, &rect.width, &rect.height);
       cam.fixedcamid = -1;
       cam.type = mjCAMERA_FREE;
-      mjr_setBuffer(mjFB_WINDOW, &con);
+      mjr_setBuffer(mjFB_WINDOW, con);
       if( con.currentBuffer!=mjFB_WINDOW )
           printf("Warning: window rendering not supported\n");
-      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-      mjr_render(window_rect, &scn, &con);
+      mjv_updateScene(m, d, opt, NULL, cam, mjCAT_ALL, scn);
+      mjr_render(window_rect, scn, con);
       glfwSwapBuffers(window);
-      return 0;
 }
 
-int close(FILE* fp, unsigned char* rgb) {
-    fclose(fp);
-    free(rgb);
+int closeMujoco(mjModel* m, mjData* d, mjrContext* con, mjvScene* scn) {
     mj_deleteData(d);
     mj_deleteModel(m);
-    mjr_freeContext(&con);
-    mjv_freeScene(&scn);
+    mjr_freeContext(con);
+    mjv_freeScene(scn);
     mj_deactivate();
 }
 
@@ -97,9 +84,19 @@ int main(int argc, const char** argv)
     int H = 800;
     int W = 800;
     char const* filepath = "humanoid.xml";
+    char const* keypath = "mjkey.txt";
+    mjModel* m;
+    mjData* d;
+    mjvScene scn;
+    mjrContext con;
+    mjvCamera cam; 
+    mjvOption opt;
 
     GLFWwindow* window = initGlfw();
-    initMujoco(filepath);
+    mj_activate(keypath);
+    m = loadModel(filepath);
+    d = mj_makeData(m);
+    initMujoco(m, d, &scn, &cam, &opt, &con);
 
     // allocate rgb and depth buffers
     unsigned char* rgb = (unsigned char*)malloc(3*H*W);
@@ -113,12 +110,17 @@ int main(int argc, const char** argv)
 
     // main loop
     for( int i = 0; i < 500; i++) {
-      renderOffscreen(rgb, H, W);
+      renderOffscreen(rgb, H, W, m, d, &cam);
       fwrite(rgb, 3, H * W, fp);
-      renderOnscreen(window);
+      renderOnscreen(window, m, d, &scn, &con, &cam, &opt);
       mj_step(m, d);
     }
     printf("ffmpeg -f rawvideo -pixel_format rgb24 -video_size %dx%d -framerate 60 -i rgb.out -vf 'vflip' video.mp4\n", H, W);
 
+    fclose(fp);
+    free(rgb);
+    closeMujoco(m, d, &con, &scn);
+
     return 0;
 }
+
